bf2630a2f6c8cbbd91cf1acd2bdff398f591fc19
AmoghJM <amoghmoger@gmail.com>
2024-10-09 16:36:29 +0530

    Fixed bugs

--- script.js
+++ script.js
@@ -13,24 +13,21 @@ let secCounter = 0;
 let minCounter = 0;
 let centiSecCounter = 0;
 let lapItem = 0;
-let isReset = false;
+let elapsedTime = 0;
+let sec;
 
-let sec, centiSec;
-
-let startTime, elapsedTime = 0;
-
-const formatTime = (time) => time < 10 ? `0${time}` : time;
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
 
 const toggleButton = () => {
     lapButton.classList.remove("hidden");
     resetButton.classList.remove("hidden");
-}
+};
 
 const updateTimeDisplay = () => {
     const totalElapsed = Date.now() - startTime + elapsedTime;
-    const totalCentiSecs = Math.floor(totalElapsed / 10); 
+    const totalCentiSecs = Math.floor(totalElapsed / 10);
     centiSecCounter = totalCentiSecs % 100;
-    secCounter = Math.floor(totalCentiSecs / 100) % 60; 
+    secCounter = Math.floor(totalCentiSecs / 100) % 60;
     minCounter = Math.floor(totalCentiSecs / 6000);
 
     minutes.innerHTML = `${formatTime(minCounter)} :&nbsp;`;
@@ -41,29 +38,20 @@ const updateTimeDisplay = () => {
 const play = () => {
     if (!isPlay) {
         playButton.innerHTML = 'Pause';
-
         bg.classList.add("animation-bg");
         bg.style.animationPlayState = 'running';
-
         startTime = Date.now();  
-
         sec = setInterval(updateTimeDisplay, 10); 
-        
         isPlay = true;
-        isReset = true;
     } else {
         playButton.innerText = 'Play';
         clearInterval(sec); 
-
         elapsedTime += Date.now() - startTime; 
-
-        bg.style.animationPlayState = 'paused'; 
-
+        bg.style.animationPlayState = 'paused';
         isPlay = false;
     }
     toggleButton();
-}
-
+};
 
 
 const reset = () => {
@@ -75,15 +63,16 @@ const reset = () => {
     elapsedTime = 0;
 
     isPlay = false;
-    isReset = false;
 
     lapButton.classList.add("hidden");
     resetButton.classList.add("hidden");
+    clearButton.classList.add("hidden"); 
 
     seconds.innerHTML = '&nbsp;00 :';
     centiSecond.innerHTML = '&nbsp;00';
     minutes.innerHTML = '00 :';
-}
+    laps.innerHTML = '';
+};
 
 const lap = () => {
     const li = document.createElement("li");
@@ -94,21 +83,22 @@ const lap = () => {
     number.setAttribute("class", "number");
     timeStamp.setAttribute("class", "time-stamp");
 
-    number.innerText = `#${++lapItem}`
+    number.innerText = `#${++lapItem}`;
     timeStamp.innerHTML = `${formatTime(minCounter)} : ${formatTime(secCounter)} : ${formatTime(centiSecCounter)}`;
 
     li.append(number, timeStamp);
     laps.append(li);
 
     clearButton.classList.remove("hidden");
-}
+};
 
 const clearAll = () => {
-    laps.innerHTML = '';
-    laps.append(clearButton);
+    laps.innerHTML = ''; 
     clearButton.classList.add("hidden");
     lapItem = 0;
-}
+};
+
+clearButton.classList.add("hidden");
 
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
